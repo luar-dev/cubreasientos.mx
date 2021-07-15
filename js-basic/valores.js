6e502d08e550bbb25efed6deb0ac9fe68aa072a1
Cubreasientos Sales Person <contacto@cubreasientos.mx>
2021-07-15 00:20:25 -0500

    added estudiantes 'e' and changed raul's age

--- js-basic/valores.js
+++ js-basic/valores.js
@@ -1,11 +1,11 @@
-function newFunction(name = 'Raul', age = 34, country = 'MX') {
+function newFunction(name = 'Raul', age = 24, country = 'MX') {
     console.log(name, age, country);
 }
 
 newFunction();
 newFunction('Jessica', 25, 'US');
 
-var estudiantes = ["a", "b", "c", "d"];
+var estudiantes = ["a", "b", "c", "d", "e"];
 
 function saludar(estudiante) {
     console.log(`letra ${estudiante}`)
